a10b372ee77dd64f90b54c860cc52adf335b86aa
fretzestavillo <estavillofretz@gmail.com>
2024-11-08 18:42:07 +0800

    exercise completed

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,4 +1,5 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import { BoxAndQoutes } from "./box-and-qoutes";
 
 export default function Home() {
   return (
@@ -6,13 +7,31 @@ export default function Home() {
       <Stack
         sx={{
           display: "flex",
-          justifyContent: "top",
+          justifyContent: "center",
           alignItems: "center",
-          color: "white",
           height: "100vh",
         }}
       >
-        Get Inspired App
+        <Stack
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            backgroundColor: "#e0e0e0",
+            width: 400,
+            height: 400,
+            borderRadius: 2,
+            gap: 4,
+            padding: 5,
+          }}
+        >
+          <Typography sx={{ fontFamily: "Roboto", fontSize: 23 }}>
+            Get Inspired App
+          </Typography>
+
+          <BoxAndQoutes />
+        </Stack>
       </Stack>
     </>
   );
