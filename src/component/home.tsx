beed3b92b59eb28a523ea515450339c07285aaa9
fretzestavillo <estavillofretz@gmail.com>
2024-11-08 16:12:30 +0800

    install material ui

---
+++ src/component/home.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Stack
+        sx={{
+          display: "flex",
+          justifyContent: "top",
+          alignItems: "center",
+          color: "white",
+          height: "100vh",
+        }}
+      >
+        Get Inspired App
+      </Stack>
+    </>
+  );
+}
